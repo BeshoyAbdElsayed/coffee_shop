01fd11e9fe88d24a1d27c3e91e6391ebeb275a5c
Beshoy Abd Elsayed <beshoabdo8@gmail.com>
2021-04-16 00:17:06 +0200

    export postman collection

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -171,14 +171,8 @@ def requires_auth(permission=''):
         @wraps(f)
         def wrapper(*args, **kwargs):
             token = get_token_auth_header()
-            try:
-                payload = verify_decode_jwt(token)
-            except:
-                 raise AuthError({
-                    'code': 'authorization_failed',
-                    'description': 'Authorization failed'
-                }, 401)
-
+            payload = verify_decode_jwt(token)
+        
             check_permissions(permission, payload)
             return f(*args, **kwargs)
 
